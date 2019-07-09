15570644108efe6f42ff0e15bcd5d662d7fc83e9
Liz <moraleslizzy24@gmail.com>
2019-07-09 14:58:34 -0700

    Initial Commit

---
+++ example.js
@@ -0,0 +1,16 @@
+ // alert("Learn more about benefits of Recycling");
+
+function bigImg(x){
+  x.style.height = "400px";
+  x.style.width = "400px";
+}
+// function originalImg(x){
+//   x.style.height = "70px";
+//   x.style.width = "70px";
+// }
+
+function video(){
+
+
+
+}
